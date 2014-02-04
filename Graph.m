d76a402403d8c6cc67ff3458e386808be2bf40d6
Joshua Ferguson <jfergusone@gmail.com>
2014-02-04 12:59:26 -0800

    Updated graph methods and implemented stochastic matrix check function

--- Graph.m
+++ Graph.m
@@ -10,18 +10,38 @@ classdef Graph
 		V;
 		% Edges represented by a m by 2 matrix of edges inputted into the program
 		E;
-	end
-
-
-	% all of the methods for graph class
+    end
+    % basic constructor method
 	methods
-
-		% constructor method
 		function obj = Graph(V, E)
 			obj.V = V;
 			obj.E = E;
-		end
-	end
+        end
+    end
+    % method to generate a ring graph
+    methods
+        function obj = Ring(n)
+            obj.V = (1:n);
+            obj.E = zeros(n, 2);
+            for i = 1: n-1
+                obj.E(i, 1) = i;
+                obj.E(i, 2) = i+1;
+            end
+            obj.E(n, 1) = n;
+            obj.E(n, 2) = 1;
+        end
+    end
+   % method to generate a grid graph with m rows and n columns
+   methods 
+        function obj = Grid(m, n)
+            obj.V = (1:(m*n));
+            k = (m-1)*n + m*(n-1);
+            obj.E = zeros(k, 2);
+            %create the horozontal edges
+            %create the verticle edges
+        end
+    end
+    
 
 
 	% a ring graph - simple method to make one
