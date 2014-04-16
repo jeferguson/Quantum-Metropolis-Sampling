c2d1ae65291b3585e6dc6974912b13d5a06a1e51
Joshua Ferguson <jfergusone@gmail.com>
2014-04-16 15:48:38 -0700

    QC_script update

--- QC_script.m
+++ QC_script.m
@@ -3,14 +3,22 @@
 q1 = [-1 0 0 0; 0 0 -1 0; 0 -1 0 0; 0 0 0 -1];
 
 
-% going to try different values for g
+% going to try different values for beta
+beta = 5;
 g = 5;
 
 q2 = [0 0 0 0; 0 g/2 1 0; 0 1 g/2 0; 0 0 0 0];
 
+t = [0 1; 1 0];
+
+deltas = getn_deltas(q2, t, 13,15, beta)
+
+
+% ***************************************
+% ALL OLD SCRIPTS I USED FOR TESTING BELOW
+% ***************************************
 
 % the transform which we use to calculate the stochastic matrix
-t = [0 1; 1 0];
 %{
 G = Ring(7);
 
